29db1f07d429276c54344625aac44d1a79d312ae
UTSAV0175 <55583642+UTSAV0175@users.noreply.github.com>
2019-10-12 04:53:23 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -8,3 +8,14 @@ int main()
     for(i=2;i<n;i++)
         prod=prod*i;
     printf("factorial %d",prod);
+    #include<stdio.h>
+int main()
+
+{
+    int n,i;
+    scanf("%d",&n);
+    int prod=1;
+    for(i=2;i<n;i++)
+        prod=prod*i;
+    printf("factorial %d",prod);
+
